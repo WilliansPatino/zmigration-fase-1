00c15260f07a24a194272f934ba743344ef48660
Willians Patiño <ppwj@yahoo.com>
2023-11-18 23:37:36 -0400

    ajustes

--- 00_set-route.sh
+++ 00_set-route.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
+
 SAVETO="/tmp/MIGRATION/"
-FILE="estructura-ldap.ldif"
 
-/opt/zimbra/libexec/zmslapcat -c $SAVETO$FILE
+mkdir $SAVETO
 
-ls -lpa $SAVETO
+cp $HOME/zmigration-fase-1/01_get-ldapinfo.sh $SAVETO
 
+chown  zimbra:zimbra $SAVETO
+
+chmod -R 774  $SAVETO/01_get-ldapinfo.sh
+chown zimbra:zimbra  $SAVETO/01_get-ldapinfo.sh
 
 
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # zmigration-fase-1
 
     Fase 1 - Migración Zimbra
+
+
+* Paso 1
+
+'''    
+[root@webmail ~]#
+
+'''
+
+* Paso 2
+
+'''    
+[root@webmail ~]#
+
+'''
