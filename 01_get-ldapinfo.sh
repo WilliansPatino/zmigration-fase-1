f7bf10154809bdded2aba818675f07037540d8fb
Willians Patiño <ppwj@yahoo.com>
2023-11-19 01:47:31 -0400

    Ajustes

--- 01_get-ldapinfo.sh
+++ 01_get-ldapinfo.sh
@@ -7,9 +7,5 @@ FILE="ldap-info.ldif"
 /opt/zimbra/libexec/zmslapcat $SAVETO
 ls -lpa $SAVETO
 
-echo "Subject: Esctructura LDAP" > $SAVETO/email.txt
-echo " Favor enviar al programador a esta dirección: " >>  $SAVETO/email.txt
-echo "      expert-itech@outlook.com " >>  $SAVETO/email.txt
-echo "/n Gracias" >>  $SAVETO/email.txt
-sendmail -v admin@proviasdes.gob.pe < $SAVETO/email.txt
+echo "Subject: Estructura LDAP" | sendmail -v admin@proviasdes.gob.pe < $SAVETO/ldap.bak
 
